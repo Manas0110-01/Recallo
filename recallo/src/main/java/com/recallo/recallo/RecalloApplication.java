9e8ac6fe7eaafe87a1fbabdfee8ec0423bbf1ad0
Manas0110-01 <yadavmanas1217@gmail.com>
2026-04-12 14:38:41 +0530

    Initial Spring Boot setup

---
+++ recallo/src/main/java/com/recallo/recallo/RecalloApplication.java
@@ -0,0 +1,13 @@
+package com.recallo.recallo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecalloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecalloApplication.class, args);
+	}
+
+}
---
+++ recallo/src/test/java/com/recallo/recallo/RecalloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recallo.recallo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecalloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
